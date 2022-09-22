de0c5b89bed7df12e6b0c34d338340262aa0acfe
André R. B. <bronca.andre@gmail.com>
2022-09-22 10:02:24 -0300

    alterando o model pessoa

--- src/com/arb/mod06/model/Pessoa.java
+++ src/com/arb/mod06/model/Pessoa.java
@@ -3,6 +3,7 @@ package com.arb.mod06.model;
 public class Pessoa {
 	private String nome;
 	private String sobrenome;
+	private int idade;
 
 	public String getNome() {
 		return nome;
@@ -18,5 +19,13 @@ public class Pessoa {
 
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 }
