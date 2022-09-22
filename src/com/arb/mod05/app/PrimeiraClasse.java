8674a7844d74b33a4ab34e1642a2a253e8d13bdc
André R. B. <bronca.andre@gmail.com>
2022-09-21 23:20:26 -0300

    asdfas

--- src/com/arb/mod05/app/PrimeiraClasse.java
+++ src/com/arb/mod05/app/PrimeiraClasse.java
@@ -3,7 +3,7 @@ package com.arb.mod05.app;
 public class PrimeiraClasse {
 	public static void main(String[] args) {
 		System.out.println("hum!!!");
-		System.out.println("ta osso");
+	
 		System.out.println("gerado token");
 		
 	}
