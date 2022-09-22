ed3a64d1ddc499869380691141c2d88b4b93408b
André R. B. <bronca.andre@gmail.com>
2022-09-22 10:00:28 -0300

    Model Pessoa

---
+++ src/com/arb/mod06/model/Pessoa.java
@@ -0,0 +1,22 @@
+package com.arb.mod06.model;
+
+public class Pessoa {
+	private String nome;
+	private String sobrenome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+}
