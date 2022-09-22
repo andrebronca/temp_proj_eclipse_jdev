18ba3eb75d0c0cd014ceeeda766f8e808cb7d251
André R. B. <bronca.andre@gmail.com>
2022-09-21 23:22:21 -0300

    testes

--- src/com/arb/mod05/app/PrimeiraClasse.java
+++ src/com/arb/mod05/app/PrimeiraClasse.java
@@ -2,7 +2,7 @@ package com.arb.mod05.app;
 
 public class PrimeiraClasse {
 	public static void main(String[] args) {
-		System.out.println("hum!!!");
+		
 	
 		System.out.println("gerado token");
 		
